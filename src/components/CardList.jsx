93b3ea3080d224a58f3a83eba50e01ad12a98959
Becky <nkes50723@gmail.com>
2023-04-30 12:26:02 +0800

    fix get api

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -73,7 +73,7 @@ export const CardList = ({ inputValue, selectedRegion, onCardClick }) => {
       }
     }
     getAllCountriesAsync()
-  })
+  }, [currentPage, resetPage])
 
   useEffect(() => {
     setResetPage(true)
